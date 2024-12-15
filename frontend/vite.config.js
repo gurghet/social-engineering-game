58c154c9bc1d245406b0bdc5712a07b6faa36df5
Andrea Passaglia <andrea.passaglia@getvisibility.com>
2024-12-15 20:37:30 +0100

    works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiTargets = {
+  e2e: 'http://server:8080',
+  development: 'http://localhost:23925',
+  production: 'http://backend'
+}
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.NODE_ENV === 'development' ? 'http://localhost:23925' : 'http://backend',
+        target: apiTargets[process.env.NODE_ENV] || apiTargets.production,
         changeOrigin: true
       }
     }
